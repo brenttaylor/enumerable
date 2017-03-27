b35d9af268a2319ad5ab8c022c3eaea6d42df54e
Ryan (Brent) Taylor <btaylor@fuzzylogicstudios.com>
2017-03-26 22:08:58 -0700

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+
+setup(
+    name='enumerable',
+    packages=['enumerable'],
+    version='0.1',
+    description='An improvement on Python\'s map, filter and reduce.',
+    author='Ryan "Brent" Taylor',
+    author_email='btaylor@fuzzylogicstudios.com',
+    url='https://github.com/brenttaylor/enumerable',
+    download_url='https://github.com/brenttaylor/enumerable/archive/release-0.1.tar.gz',
+    keywords=['functools', 'map', 'filter', 'reduce'],
+    classifiers=['Programming Language :: Python :: 3'],
+)
