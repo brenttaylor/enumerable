c0486f18c5e610be1b4c277af1ff70ac0027e013
Ryan "Brent" Taylor <btaylor@fuzzylogicstudios.com>
2017-03-28 22:41:18 -0700

    Bumped release number.

--- setup.py
+++ setup.py
@@ -4,12 +4,13 @@ from distutils.core import setup
 setup(
     name='enumerable',
     packages=['enumerable'],
-    version='0.1',
+    version='0.2',
+    install_requires=['future'],
     description='An improvement on Python\'s map, filter and reduce.',
     author='Ryan "Brent" Taylor',
     author_email='btaylor@fuzzylogicstudios.com',
     url='https://github.com/brenttaylor/enumerable',
-    download_url='https://github.com/brenttaylor/enumerable/archive/release-0.1.tar.gz',
+    download_url='https://github.com/brenttaylor/enumerable/archive/release-0.2.tar.gz',
     keywords=['functools', 'map', 'filter', 'reduce'],
     classifiers=['Programming Language :: Python :: 3'],
 )
