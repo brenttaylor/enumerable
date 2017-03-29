203704c534fca0b3fb21ad4d594bc6fbca9e7700
Ryan "Brent" Taylor <btaylor@fuzzylogicstudios.com>
2017-03-28 22:08:36 -0700

    Refactor out dummy functions

--- enumerable/iterators.py
+++ enumerable/iterators.py
@@ -1,17 +1,4 @@
-import functools
-from re import search
-
-
-def Map():
-    pass
-
-
-def Filter():
-    pass
-
-
-def Reduce():
-    pass
+from functools import reduce
 
 
 class Enumerable:
@@ -22,28 +9,28 @@ class Enumerable:
         return self._iterable.__iter__()
 
     def filter(self, func, *args, **kwargs):
-        return Filter(self._iterable, func, *args, **kwargs)
+        return Enumerable((x for x in self._iterable if func(x, *args, **kwargs)))
 
     def map(self, func, *args, **kwargs):
-        return Map(self._iterable, func, *args, **kwargs)
+        return Enumerable((func(x, *args, **kwargs) for x in self._iterable))
 
-    def reduce(self, func, *args, **kwargs):
-        return Reduce(self._iterable, func, *args, **kwargs)
+    def reduce(self, func, initializer=0, *args, **kwargs):
+        def reduce_func(accumulator, value):
+            return func(accumulator, value, *args, **kwargs)
+
+        return reduce(reduce_func, self._iterable, initializer)
 
     def to(self, container_type):
         return container_type(self._iterable)
 
 
 def Filter(iterable, func, *args, **kwargs):
-    return Enumerable((x for x in iterable if func(x, *args, **kwargs)))
+    return Enumerable(iterable).filter(func, *args, **kwargs)
 
 
 def Map(iterable, func, *args, **kwargs):
-    return Enumerable((func(x, *args, **kwargs) for x in iterable))
+    return Enumerable(iterable).map(func, *args, **kwargs)
 
 
 def Reduce(iterable, func, initializer=0, *args, **kwargs):
-    def reduce_func(accumulator, value):
-        return func(accumulator, value, *args, **kwargs)
-
-    return functools.reduce(reduce_func, iterable, initializer)
+    return Enumerable(iterable).reduce(func, initializer, *args, **kwargs)
